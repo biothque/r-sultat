fb1eaa8ef4b3da8af1be114a2486409494731d48
biothque <afdc2010.cd@gmail.com>
2026-01-11 13:07:48 +0100

    Create empty info section in info.js
    Add empty info section to info.js for future content.

---
+++ info.js
@@ -0,0 +1,6 @@
+const infoSection = document.getElementById('section-info');
+// Cette section reste vide comme demandé pour vos futurs contenus
+infoSection.innerHTML = `
+    <section class="info-container">
+        </section>
+`;
