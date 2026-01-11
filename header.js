91989aab5a592709f0a7429ad56225b40e42360e
biothque <afdc2010.cd@gmail.com>
2026-01-11 13:06:14 +0100

    Add header section to header.js

---
+++ header.js
@@ -0,0 +1,12 @@
+const headerSection = document.getElementById('section-header');
+headerSection.innerHTML = `
+    <header>
+        <div class="header-logo">
+            <img src="logo.png" alt="Logo" style="height:100%;">
+        </div>
+        <div class="header-titre">
+            <h1>TITRE DU MINISTÈRE À DÉFINIR</h1>
+            <p>SOUS-TITRE OU SLOGAN</p>
+        </div>
+    </header>
+`;
