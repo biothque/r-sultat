d26d1edc07396b24653e96879dc166fb13b4c4e7
biothque <afdc2010.cd@gmail.com>
2026-01-11 13:01:44 +0100

    Create footer.js

---
+++ footer.js
@@ -0,0 +1,17 @@
+// js/footer.js
+const footerContainer = document.getElementById('section-footer');
+footerContainer.innerHTML = `
+    <footer>
+        <div class="footer-content">
+            <img src="votre-logo.png" alt="Logo" class="footer-logo">
+            <p>Plateforme des résultats des examens</p>
+            <div class="social-links">
+                <a href="mailto:contact@exemple.com">Email</a>
+                <a href="#">Facebook</a>
+                <a href="#">WhatsApp</a>
+                <a href="#">Chaîne WhatsApp</a>
+                <a href="#">TikTok</a>
+            </div>
+        </div>
+        <div class="footer-bar"></div> </footer>
+`;
