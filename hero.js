d255cd3c034e8930cce1530fcf58d8f31a7a9a53
biothque <afdc2010.cd@gmail.com>
2026-01-11 13:06:42 +0100

    Add hero section to display exam results

---
+++ hero.js
@@ -0,0 +1,9 @@
+const heroSection = document.getElementById('section-hero');
+heroSection.innerHTML = `
+    <section class="hero-banner">
+        <div class="hero-text">
+            Consultez vos résultats <br>
+            de l'Examen d'Etat
+        </div>
+    </section>
+`;
